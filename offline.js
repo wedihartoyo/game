cc72eb9cbb31dae9ce4c08e1d2b7189d4fa9fb2a
wedihartoyo <31341889+wedihartoyo@users.noreply.github.com>
2017-08-29 14:12:39 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1503982578,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/peswat-sheet0.png",
+		"images/peluru-sheet0.png",
+		"images/pesawat-sheet0.png",
+		"images/meteor-sheet0.png",
+		"images/ledakanmeteor.png",
+		"images/ledakanpesawat.png",
+		"images/gameover-sheet0.png",
+		"images/background-sheet0.png",
+		"media/sounddestroy.ogg"
+	]
+}
